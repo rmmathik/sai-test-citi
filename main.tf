979700642c76513dc07a8bbdded77f2cd0b356ac
Mathikumar <rmmathik@amazon.co.uk>
2021-01-29 19:00:30 +0000

    Om Sai Ram

--- main.tf
+++ main.tf
@@ -5,6 +5,16 @@ provider "aws" {
   region = "us-east-2"
 }
 
+terraform {
+  backend "remote" {
+    organization = "citi-demo-project"
+
+    workspaces {
+      name = "citi-workspace"
+    }
+  }
+}
+
 resource "aws_instance" "sai-inst" {
   ami = "ami-06abd2056542057e1"
   instance_type = "t2.micro"
